3e93524e8ba1d22fa935097d23a0eb39960fd90a
Niklas D <niklas.demel1@gmail.com>
2024-10-23 15:58:58 +0200

    async wip

--- code-green-m5stack.ino
+++ code-green-m5stack.ino
@@ -8,6 +8,8 @@
 
 FakeTempSensor fakeSensor = FakeTempSensor();
 String temp_value;
+int counter;
+SemaphoreHandle_t counterMutex;
 
 void fake_init() {
   temp_value = String("temp: ");
@@ -31,13 +33,36 @@ void read_test() {
 // # TEST AND FAKE STUFF #
 // #######################
 
+
+void task1(void* pvParameters) { 
+    while (1) {
+      counter++;
+      //delay(1000);
+      vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    }
+}
+
+void task2(void* pvParameters) { 
+    while (1) {  
+      M5.update();
+      read_test();
+      M5.lcd.setCursor(64,64);
+      M5.lcd.print("Counter" + counter);
+      //delay(1000); 
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void setup() {
   M5.begin();
   M5.Power.begin();
   fake_init();
+  //xTaskCreatePinnedToCore(task1, "task1", 4096, NULL, 1, NULL, 0);
+  //xTaskCreatePinnedToCore(task2, "task2", 4096, NULL, 1, NULL, 1);
 }
 
 void loop() {
   M5.update();
   read_test();
+
 }
