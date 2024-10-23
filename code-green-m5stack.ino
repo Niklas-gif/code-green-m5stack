95300486a7de58bef65e415760f9e8564e0d07a5
Niklas D <niklas.demel1@gmail.com>
2024-10-23 16:30:48 +0200

    it works

--- code-green-m5stack.ino
+++ code-green-m5stack.ino
@@ -23,10 +23,10 @@ void fake_init() {
 }
 
 void read_test() {
-  M5.Lcd.clear();
+  //M5.Lcd.clear();
   M5.Lcd.setCursor(32,32);
   M5.Lcd.print(temp_value + fakeSensor.read() + "C");
-  delay(1000);
+  //delay(1000);
 }
 
 // #######################
@@ -35,34 +35,45 @@ void read_test() {
 
 
 void task1(void* pvParameters) { 
-    while (1) {
-      counter++;
-      //delay(1000);
-      vTaskDelay(1000 / portTICK_PERIOD_MS); 
-    }
-}
-
-void task2(void* pvParameters) { 
-    while (1) {  
-      M5.update();
-      read_test();
-      M5.lcd.setCursor(64,64);
-      M5.lcd.print("Counter" + counter);
-      //delay(1000); 
+    Serial.println("Task 1 started");
+    for (;;) {
+      if (xSemaphoreTake(counterMutex, portMAX_DELAY)) {
+        counter++;
+        xSemaphoreGive(counterMutex);
+      }
       vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
+void task2(void* pvParameters) { 
+    Serial.println("Task 2 started");
+    for (;;) {  
+      //M5.update();
+      if (xSemaphoreTake(counterMutex, portMAX_DELAY)) {
+        read_test();
+        M5.lcd.setCursor(32,64);
+        M5.Lcd.printf("Counter: %d", counter);
+        xSemaphoreGive(counterMutex);
+      }
+      //delay(1000); 
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      M5.Lcd.clear();
+    }
+}
+
 void setup() {
+  Serial.begin(115200);
   M5.begin();
   M5.Power.begin();
   fake_init();
-  //xTaskCreatePinnedToCore(task1, "task1", 4096, NULL, 1, NULL, 0);
-  //xTaskCreatePinnedToCore(task2, "task2", 4096, NULL, 1, NULL, 1);
+
+  counterMutex = xSemaphoreCreateMutex();
+  xTaskCreatePinnedToCore(task1, "task1", 8192, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(task2, "task2", 8192, NULL, 1, NULL, 1);
 }
 
 void loop() {
-  M5.update();
-  read_test();
+  //M5.update();
+  //read_test();
 
 }
