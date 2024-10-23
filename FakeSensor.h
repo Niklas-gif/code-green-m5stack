c3e159dfbf066ce2c3be27ac3f97112ff1b56a59
Niklas D <niklas.demel1@gmail.com>
2024-10-23 12:21:22 +0200

    Test Fake library

--- FakeSensor.h
+++ FakeSensor.h
@@ -1,4 +1,7 @@
 class FakeTempSensor {
   public:
-    float read();
+    FakeTempSensor(); //: values {4.20,32.0,15.12,7.40,1.0,0,-10,5} {}
+    double read();
+  private:
+    double values[8];
 };
--- code-green-m5stack.ino
+++ code-green-m5stack.ino
@@ -9,6 +9,15 @@ void hello_world() {
   M5.Lcd.setTextSize(4);
   M5.Lcd.setTextColor(GREEN); 
   M5.Lcd.print("CODE GREEN");
+  delay(2000);
+  M5.Lcd.clear();
+}
+
+void read_test() {
+  M5.Lcd.clear();
+  M5.Lcd.setCursor(32,32);
+  M5.Lcd.print(fakeSensor.read());
+  delay(1000);
 }
 
 void setup() {
@@ -19,4 +28,5 @@ void setup() {
 
 void loop() {
   M5.update();
+  read_test();
 }
