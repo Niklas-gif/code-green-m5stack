46b85905ca93d863c947875619e1bcb8babb2fd7
Niklas D <niklas.demel1@gmail.com>
2024-10-24 11:40:11 +0200

    counter reset button & render changes

--- code-green-m5stack.ino
+++ code-green-m5stack.ino
@@ -2,6 +2,8 @@
 #include "FakeSensor.h"
 //#include <M5UnitENV.h>
 
+//SCREEN RES H = 240 W = 320
+
 // #######################
 // # TEST AND FAKE STUFF #
 // #######################
@@ -24,8 +26,10 @@ void fake_init() {
 
 void read_test() {
   //M5.Lcd.clear();
-  M5.Lcd.setCursor(32,32);
-  M5.Lcd.print(temp_value + fakeSensor.read() + "C");
+  //M5.Lcd.setCursor(32,32);
+  //M5.Lcd.print(temp_value + fakeSensor.read() + "C");
+    M5.Lcd.fillRect(0, 32, 320 , 22, BLACK);
+    M5.Lcd.drawString(temp_value + fakeSensor.read() + "C",32,32);
   //delay(1000);
 }
 
@@ -38,7 +42,10 @@ void task1(void* pvParameters) {
     Serial.println("Task 1 started");
     for (;;) {
       if (xSemaphoreTake(counterMutex, portMAX_DELAY)) {
-        counter++;
+          if(M5.BtnA.isPressed()) {
+            counter = 0;
+          }
+          counter++;
         xSemaphoreGive(counterMutex);
       }
       vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -48,16 +55,16 @@ void task1(void* pvParameters) {
 void task2(void* pvParameters) { 
     Serial.println("Task 2 started");
     for (;;) {  
-      //M5.update();
+      M5.update();
+      read_test();
       if (xSemaphoreTake(counterMutex, portMAX_DELAY)) {
-        read_test();
-        M5.lcd.setCursor(32,64);
-        M5.Lcd.printf("Counter: %d", counter);
+        M5.Lcd.fillRect(0, 64, 320, 22, BLACK);
+        M5.Lcd.drawString("Counter:" + String(counter),32,64);
         xSemaphoreGive(counterMutex);
       }
       //delay(1000); 
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-      M5.Lcd.clear();
+      vTaskDelay(300 / portTICK_PERIOD_MS);
+      //M5.Lcd.fillScreen(BLACK);
     }
 }
 
