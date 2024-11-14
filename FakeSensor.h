37a9e69385b59c5a58ddd323719ac933365be23d
Niklas D <niklas.demel1@gmail.com>
2024-11-14 12:33:28 +0100

    removed fake sensor

--- FakeSensor.h
+++
@@ -1,7 +0,0 @@
-class FakeTempSensor {
-  public:
-    FakeTempSensor(); //: values {4.20,32.0,15.12,7.40,1.0,0,-10,5} {}
-    double read();
-  private:
-    double values[8];
-};
--- code-green-m5stack.ino
+++ code-green-m5stack.ino
@@ -1,43 +1,12 @@
 #include <M5Stack.h>
-#include "FakeSensor.h"
 //#include <M5UnitENV.h>
 
-// #######################
-// # TEST AND FAKE STUFF #
-// #######################
-
-FakeTempSensor fakeSensor = FakeTempSensor();
-String temp_value;
-
-void fake_init() {
-  temp_value = String("temp: ");
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextSize(4);
-  M5.Lcd.setTextColor(GREEN); 
-  M5.Lcd.print("CODE GREEN");
-  delay(2000);
-  M5.Lcd.setTextSize(3);
-  M5.Lcd.clear();
-}
-
-void read_test() {
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(32,32);
-  M5.Lcd.print(temp_value + fakeSensor.read() + "C");
-  delay(1000);
-}
-
-// #######################
-// # TEST AND FAKE STUFF #
-// #######################
 
 void setup() {
   M5.begin();
   M5.Power.begin();
-  fake_init();
 }
 
 void loop() {
   M5.update();
-  read_test();
 }
