d2e90724e88579fbbfa4b3ce6cfaf7ee534831c1
Niklas D <niklas.demel1@gmail.com>
2024-11-14 12:56:24 +0100

    pin setup and libs

--- code-green-m5stack.ino
+++ code-green-m5stack.ino
@@ -1,6 +1,28 @@
 #include <M5Stack.h>
-//#include <M5UnitENV.h>
+#include <Wire.h>
+#include <Digital_Light_TSL2561.h>
+#include "M5UnitENV.h" 
 
+struct Plant {
+  String name;
+  double idealTemprature;
+  double idealHumidity;
+  int idealLightCondition;
+};
+
+
+/* PINS PUMP */
+#define INPUT_PIN 36 
+#define PUMP_PIN  26
+
+/* PINS TEMP SENSOR */
+#define TEMP_SDA_PIN 21
+#define TEMP_SCL_PIN 22
+
+
+/* heat humidity sensor*/
+SHT4X sht4; 
+BMP280 bmp;
 
 void setup() {
   M5.begin();
