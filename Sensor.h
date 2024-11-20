8dd04ff5a05c99dfd5d317a1adc521f936948f55
Niklas D <niklas.demel1@gmail.com>
2024-11-20 09:32:52 +0100

    wip waterleve

--- Sensor.h
+++ Sensor.h
@@ -4,6 +4,7 @@
     double currentHumidity;
     int currentLightCondition;
     int rawADC;
+    int waterLevel; //NOCH NICHT BENUTZTEN IMPLEMENTIERUNG FEHLT
   //TODO we store all sensor Values here and use it as as global context
   } SensorValues;
 
