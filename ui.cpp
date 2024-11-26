2b6f71f4372a9b05d386cbe633bce8f8c46d7ff5
AlexandraHofUniversity <alexandra.sumera@hof-university.de>
2024-11-26 11:21:38 +0100

    UI: Sensor-Werte erweitert

--- ui.cpp
+++ ui.cpp
@@ -4,7 +4,6 @@
 #include "picture.h"
 #include "plant.h"
 
-// Farbcodes definieren
 #define DARK_GREEN  0x03E0
 #define GRAY 0x7BEF 
 #define ORANGE 0xFF6E
@@ -13,7 +12,7 @@ extern Plant plants[];
 
 Sensory sensor;
 
-String frames[] = {"LIGHT", "PUMP", "Pflanze", "Werte"};
+String frames[] = {"Licht", "Pumpe", "Pflanze", "Werte"};
 int currentFrame = 0; 
 
 int selectedPlantIndex = 0;
@@ -28,22 +27,17 @@ void initializeUI() {
     drawCurrentFrameContent(sensor); 
 }
 
-
 void drawNavigationBar() {
     M5.Lcd.startWrite();
-    //M5.Lcd.fillRect(0, 0, 320, 40, BLACK); 
-
     M5.Lcd.fillRect(0, 220, 320, 20, ORANGE);
-
     M5.Lcd.setTextColor(BLACK);
     M5.Lcd.setCursor(10, 225);  
-    M5.Lcd.print("A"); +
+    M5.Lcd.print("A"); 
     M5.Lcd.setCursor(300, 225);  
     M5.Lcd.print("C"); 
 
     for (int i = 0; i < 4; i++) {  
         int xStart = i * (320 / 4);  
-
         if (i == currentFrame) {
             M5.Lcd.fillRect(xStart, 0, 320 / 4, 30, DARK_GREEN); 
             M5.Lcd.setTextColor(WHITE);  
@@ -61,51 +55,76 @@ void drawNavigationBar() {
     M5.Lcd.endWrite();
 }
 
-
 void drawCurrentFrameContent(Sensory &sensor) {
     M5.Lcd.fillRect(0, 40, 320, 180, BLACK);
 
     if (frames[currentFrame] == "Pflanze") {
-        M5.Lcd.setCursor(10, 60);
-        M5.Lcd.setTextColor(WHITE);
-        M5.Lcd.print(plants[selectedPlantIndex].getName()); 
-        
-        if (plants[selectedPlantIndex].getName() == "Kaktus") {
-            M5.Lcd.drawBitmap(105, 55, 130, 158, kaktus);
-        } else if (plants[selectedPlantIndex].getName() == "Bonsai") {
-            M5.Lcd.drawBitmap(77, 60, 179, 152, bonsai);
-        } else if (plants[selectedPlantIndex].getName() == "Orchidee") {
-            M5.Lcd.drawBitmap(97, 60, 130, 160, orchid);
-        } else if (plants[selectedPlantIndex].getName() == "Agave") {
-            M5.Lcd.drawBitmap(97, 55, 156, 160, agave);
-        }
+        drawPlantContent();
     } 
-    
     else if (frames[currentFrame] == "PUMP") {
-        M5.Lcd.setCursor(10, 60);
-        M5.Lcd.setTextColor(WHITE);
-        M5.Lcd.print("Pumpensteuerung:");
-        M5.Lcd.setCursor(10, 90);
-        M5.Lcd.setTextColor(DARK_GREEN);
-        M5.Lcd.printf("Pumpe: %s", sensor.isPumpRunning() ? "ON" : "OFF");
+        drawPumpContent(sensor);
     } 
-    
     else if (frames[currentFrame] == "Werte") {
-        SensorValues values = sensor.read();
-        M5.Lcd.setCursor(10, 60);
-        M5.Lcd.setTextColor(WHITE);
-        M5.Lcd.print("Temperatur: ");
-        M5.Lcd.setTextColor(DARK_GREEN);
-        M5.Lcd.print(values.currentTemprature);
-        M5.Lcd.print(" C");
-        M5.Lcd.setCursor(10, 120);
-        M5.Lcd.setTextColor(WHITE);
-        M5.Lcd.print("Feuchtigkeit: ");
-        M5.Lcd.setTextColor(DARK_GREEN);
-        M5.Lcd.print(values.currentHumidity);
-        M5.Lcd.print(" %");
+        drawValuesContent(sensor);
     }
 }
+
+void drawPlantContent() {
+    M5.Lcd.setCursor(10, 60);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print(plants[selectedPlantIndex].getName()); 
+    
+    if (plants[selectedPlantIndex].getName() == "Kaktus") {
+        M5.Lcd.drawBitmap(105, 55, 130, 158, kaktus);
+    } else if (plants[selectedPlantIndex].getName() == "Bonsai") {
+        M5.Lcd.drawBitmap(77, 60, 179, 152, bonsai);
+    } else if (plants[selectedPlantIndex].getName() == "Orchidee") {
+        M5.Lcd.drawBitmap(97, 60, 130, 160, orchid);
+    } else if (plants[selectedPlantIndex].getName() == "Agave") {
+        M5.Lcd.drawBitmap(97, 55, 156, 160, agave);
+    }
+}
+
+void drawPumpContent(Sensory &sensor) {
+    M5.Lcd.setCursor(10, 60);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("Pumpensteuerung:");
+    M5.Lcd.setCursor(10, 90);
+    M5.Lcd.setTextColor(DARK_GREEN);
+    M5.Lcd.printf("Pumpe: %s", sensor.isPumpRunning() ? "ON" : "OFF");
+}
+
+void drawValuesContent(Sensory &sensor) {
+    SensorValues values = sensor.read();
+    
+    M5.Lcd.setCursor(10, 60);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("Temperatur: ");
+    M5.Lcd.setTextColor(DARK_GREEN);
+    M5.Lcd.print(values.currentTemprature);
+    M5.Lcd.print(" C");
+    
+    M5.Lcd.setCursor(10, 90);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("Feuchtigkeit: ");
+    M5.Lcd.setTextColor(DARK_GREEN);
+    M5.Lcd.print(values.currentHumidity);
+    M5.Lcd.print(" %");
+
+    M5.Lcd.setCursor(10, 120);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("Licht: ");
+    M5.Lcd.setTextColor(DARK_GREEN);
+    M5.Lcd.print(values.currentLightCondition);
+    M5.Lcd.print(" Lux");
+
+    M5.Lcd.setCursor(10, 150);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("Wasserstand: ");
+    M5.Lcd.setTextColor(DARK_GREEN);
+    M5.Lcd.print(values.waterLevel == HIGH ? "Voll" : "Leer"); 
+}
+
 
 void changeFrame(Sensory &sensor) {
     if (M5.BtnC.wasPressed()) {
@@ -124,12 +143,10 @@ void changeFrame(Sensory &sensor) {
             drawCurrentFrameContent(sensor);
         }
     } 
-    
     else if (frames[currentFrame] == "PUMP" && M5.BtnB.wasPressed()) {
         sensor.togglePump();
         drawCurrentFrameContent(sensor);
     } 
-    
     else if (frames[currentFrame] == "Werte") {
         unsigned long currentMillis = millis();
         if (currentMillis - previousMillis >= interval) {
@@ -138,10 +155,4 @@ void changeFrame(Sensory &sensor) {
             drawCurrentFrameContent(sensor);
         }
     }
-    
 }
-
-
-
-
-
--- ui.h
+++ ui.h
@@ -7,13 +7,13 @@
 enum Screen { LIGHT, PUMP, PLANT, VALUES };
 
 void initializeUI();
-
 void updateUI();
-
 void drawNavigationBar();
-
 void drawCurrentFrameContent(Sensory &sensor);
-
 void changeFrame(Sensory &sensor);
+
+void drawPlantContent();
+void drawPumpContent(Sensory &sensor);
+void drawValuesContent(Sensory &sensor);
 
 #endif
